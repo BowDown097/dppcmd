f4b727b2a1687c2bbfb85721e8cbc42cf6766836
BowDown097 <btd6modsyeet@gmail.com>
2024-08-09 22:54:50 -0700

    Change tuple_index_of value to long

--- dpp-command-handler/utils/tuple_traits.h
+++ dpp-command-handler/utils/tuple_traits.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <cstddef>
 #include <tuple>
 
 namespace dpp
@@ -9,32 +8,32 @@ namespace dpp
         template<class T, class Tuple, class Idxs>
         struct tuple_index_of_impl;
 
-        template<class T, class... Args, size_t... Is>
+        template<class T, class... Args, std::size_t... Is>
         struct tuple_index_of_impl<T, std::tuple<Args...>, std::index_sequence<Is...>>
         {
-            static constexpr size_t value = []() {
-                size_t result = -1;
+            static constexpr long value = []() {
+                long result = -1;
                 ((std::same_as<T, std::tuple_element_t<Is, std::tuple<Args...>>> && (result = Is)), ...);
                 return result;
             }();
         };
 
         template<class T, class Tuple>
-        struct tuple_index_of { static constexpr size_t value = -1; };
+        struct tuple_index_of { static constexpr long value = -1; };
 
         template<class T>
-        struct tuple_index_of<T, std::tuple<>> { static constexpr size_t value = -1; };
+        struct tuple_index_of<T, std::tuple<>> { static constexpr long value = -1; };
 
         template<class T, class... Args>
         struct tuple_index_of<T, std::tuple<Args...>> {
             using Tuple = std::tuple<std::remove_cv_t<Args>...>;
-            static constexpr size_t value = tuple_index_of_impl<T, Tuple, std::index_sequence_for<Args...>>::value;
+            static constexpr long value = tuple_index_of_impl<T, Tuple, std::index_sequence_for<Args...>>::value;
         };
 
         template<class T, class Tuple>
-        constexpr size_t tuple_index_of_v = tuple_index_of<T, Tuple>::value;
+        constexpr long tuple_index_of_v = tuple_index_of<T, Tuple>::value;
 
-        template<size_t N, class Tuple>
+        template<std::size_t N, class Tuple>
         struct tuple_drop_nth {};
 
         template<typename T, typename... Ts>
@@ -43,7 +42,7 @@ namespace dpp
             using type = std::tuple<Ts...>;
         };
 
-        template<size_t N, typename T, typename... Ts>
+        template<std::size_t N, typename T, typename... Ts>
         struct tuple_drop_nth<N, std::tuple<T, Ts...>>
         {
             using type = decltype(std::tuple_cat(
@@ -51,7 +50,7 @@ namespace dpp
                 std::declval<typename tuple_drop_nth<N - 1, std::tuple<Ts...>>::type>()));
         };
 
-        template<size_t N, class Tuple>
+        template<std::size_t N, class Tuple>
         using tuple_drop_nth_t = typename tuple_drop_nth<N, Tuple>::type;
 
         template<class Tuple>
